c6ae76c7201f9daedf6985438b087e309563bc48
Arjan van de Ven <arjan@linux.intel.com>
2020-03-03 17:21:40 +0000

    reduce noise in review some more

--- patchfilter.py
+++ patchfilter.py
@@ -278,6 +278,7 @@ def main():
 
     zap_line_in_file("b/options.conf", "archives = ")
     zap_line_in_file("b/options.conf", "giturl = ")
+    zap_line_in_file("b/options.conf", "url = ")
     zap_line_in_file("b/options.conf", "[package]")
     zap_line_in_file("b/options.conf", "")
     zap_line_in_file("b/options.conf", "[autospec]")
